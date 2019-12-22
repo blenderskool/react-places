ba3bcfcb8e51ed44a8c37ff9c0bf851c8b51f205
blenderskool <akash.hamirwasia@gmail.com>
2019-12-22 21:54:17 +0530

    Added searching

--- src/app.js
+++ src/app.js
@@ -1,14 +1,63 @@
 import React from 'react';
+import { Search } from 'js-search';
 import Map from './components/map';
 import Sidebar from './components/sidebar';
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Sidebar />
-      <Map />
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.search = new Search('_id');
+    this.search.addIndex('placeName');
+    this.state = {
+      errMessage: '',
+      places: []
+    };
+  }
+
+  componentDidMount() {
+
+    fetch('https://staging.23ml.in/api/places', {
+      headers: {
+        'idtoken': process.env.REACT_APP_23ML_KEY 
+      }
+    })
+    .then(res => res.json())
+    .then(places => {
+      this.search.addDocuments(places);
+      this.setState({ places });
+    })
+    .catch(err => {
+      this.setState({ errMessage: err });
+    })
+  }
+
+  handleSearch(e) {
+    const searchPlaces = this.search.search(e.target.value);
+
+    this.setState({
+      places: searchPlaces.length ? searchPlaces : this.search._documents
+    });
+  }
+
+  render() {
+
+    return (
+      <div className="wrapper">
+        {
+          this.state.errMessage ?
+          <div>{this.state.errMessage}</div> :
+          <React.Fragment>
+            <Sidebar
+              onSearch={this.handleSearch.bind(this)}
+              places={this.state.places}
+            />
+            <Map places={this.state.places} />
+          </React.Fragment>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 import './sidebar.css';
 
 export default class Sidebar extends React.Component {
-  
+
   render() {
+    const { places, onSearch } = this.props;
 
     return (
       <div className="sidebar">
         <input
           type="search"
           placeholder="Search place..."
+          onChange={onSearch}
         />
 
         <ul className="places">
           {
-            this.props.places.map(place => (
+            places.map(place => (
               <li key={place._id}>
                 {place.placeName}
               </li>
