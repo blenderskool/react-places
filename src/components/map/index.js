85a946fe84f5a3eea00021887ab42ec8ffffe61f
blenderskool <akash.hamirwasia@gmail.com>
2019-12-22 14:09:39 +0530

    Refactored to stateless components

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -6,48 +6,35 @@ function Marker({ placeName }) {
   return <div className="marker" tabIndex="-1" dataname={placeName} />;
 }
 
-export default class Map extends React.Component {
+export default function Map({ places, marker, onClick}) {
 
-  static defaultProps = {
-    center: {
-      lat: 26.19,
-      lng: 91.69
-    },
-    zoom: 15
-  };
-
-  render() {
-    const { places, marker, onClick, center, zoom } = this.props;
-
-    return (
-      <div className="map">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_KEY }}
-          defaultCenter={center}
-          defaultZoom={zoom}
-          onClick={onClick}
-        >
-          {
-            places.map(place => (
-              <Marker
-                key={place.placeName}
-                lat={place.location.Lat}
-                lng={place.location.Lng}
-                placeName={place.placeName}
-              />
-            ))
-          }
-          {
-            marker ?
+  return (
+    <div className="map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_KEY }}
+        defaultCenter={{ lat: 26.19, lng: 91.69 }}
+        defaultZoom={15}
+        onClick={onClick}
+      >
+        {
+          places.map(place => (
             <Marker
-              lat={marker.lat ? marker.lat : 0}
-              lng={marker.lng ? marker.lng : 0}
-            /> :
-            null
-          }
-        </GoogleMapReact>
-      </div>
-    );
-  }
-
+              key={place.placeName}
+              lat={place.location.Lat}
+              lng={place.location.Lng}
+              placeName={place.placeName}
+            />
+          ))
+        }
+        {
+          marker ?
+          <Marker
+            lat={marker.lat ? marker.lat : 0}
+            lng={marker.lng ? marker.lng : 0}
+          /> :
+          null
+        }
+      </GoogleMapReact>
+    </div>
+  );
 }
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,36 +1,32 @@
 import React from 'react';
 import './sidebar.css';
 
-export default class Sidebar extends React.Component {
+export default function Sidebar({ places, onSearch, onAddPlace }) {
 
-  render() {
-    const { places, onSearch, onAddPlace } = this.props;
+  return (
+    <div className="sidebar">
+      <input
+        type="search"
+        placeholder="Search place..."
+        onChange={onSearch}
+      />
+      
+      <ul className="places">
+        {
+          places.length ?
+          places.map(place => (
+            <li key={place.placeName}>
+              {place.placeName}
+            </li>
+          )) :
+          'No place found'
+        }
+      </ul>
 
-    return (
-      <div className="sidebar">
-        <input
-          type="search"
-          placeholder="Search place..."
-          onChange={onSearch}
-        />
-        
-        <ul className="places">
-          {
-            places.length ?
-            places.map(place => (
-              <li key={place.placeName}>
-                {place.placeName}
-              </li>
-            )) :
-            'No place found'
-          }
-        </ul>
-
-        <button className="btn add-place" onClick={onAddPlace}>
-          Add place
-          <i className="icon ion-md-add" />
-        </button>
-      </div>
-    );
-  }
+      <button className="btn add-place" onClick={onAddPlace}>
+        Add place
+        <i className="icon ion-md-add" />
+      </button>
+    </div>
+  );
 }
